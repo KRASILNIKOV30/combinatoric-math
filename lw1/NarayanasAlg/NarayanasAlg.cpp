352e5088cf2afee015bbd73a06180adbbe4c8c65
Krasilnikov30 <79829503+KRASILNIKOV30@users.noreply.github.com>
2022-02-15 16:00:30 +0300

    Выполнил первое задание

--- lw1/NarayanasAlg/NarayanasAlg.cpp
+++ lw1/NarayanasAlg/NarayanasAlg.cpp
@@ -9,13 +9,13 @@
 
 int main()
 {
-    std::vector<size_t> v = { 7, 1, 12, 5, 9, 20, 15 };
+    std::vector<size_t> v = { 7, 1, 12, 5, 9, 20, 15, 4, 8, 6 };
     boost::timer t;
     t.restart();
     std::sort(v.begin(), v.end());
     do
     {
-        copy(v.begin(), v.end(), std::ostream_iterator<size_t>(std::cout, " "));
+        //copy(v.begin(), v.end(), std::ostream_iterator<size_t>(std::cout, " "));
         std::cout << std::endl;
     } while (std::next_permutation(v.begin(), v.end()));
     double duration = t.elapsed();
