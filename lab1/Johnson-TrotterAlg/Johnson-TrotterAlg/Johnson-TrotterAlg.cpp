c7c5f141a85d6d9b6e8550611a5c9acc60fb96de
krasilnikov30 <bkrasilnikov3006@gmail.com>
2024-02-07 09:12:00 +0300

    Рефакторинг структуры репозитория: выделил каждое задание в отдельное решение

---
+++ lab1/Johnson-TrotterAlg/Johnson-TrotterAlg/Johnson-TrotterAlg.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <vector>
+#define BOOST_TIMER_ENABLE_DEPRECATED
+#include <boost/timer.hpp>
+
+void TimeCount(double duration)
+{
+    long double d = duration;
+    for (int i = 11; i <= 15; i++)
+    {
+        d *= i;
+    }
+    d /= 3600;
+    std::cout << "N = 15: " << d << " hours" << std::endl;
+
+    for (int i = 16; i <= 20; i++)
+    {
+        d *= i;
+    }
+    d /= 24;
+    std::cout << "N = 20: " << d << " days" << std::endl;
+
+    for (int i = 21; i <= 50; i++)
+    {
+        d *= i;
+    }
+    d /= 365;
+    std::cout << "N = 50: " << d << " years" << std::endl;
+
+    for (int i = 51; i <= 100; i++)
+    {
+        d *= i;
+    }
+    std::cout << "N = 100: " << d << " years" << std::endl;
+}
+
+int main()
+{
+    boost::timer t;
+    t.restart();
+
+    const int N = 10;
+
+    std::vector<int> cur;
+    std::vector<int> rev;
+    std::vector<int> dir;
+    cur.resize(N + 2);
+    rev.resize(N + 2);
+    dir.resize(N + 2);
+    for (int i = 1; i <= N; i++)
+    {
+        cur[i] = i;
+        rev[i] = i;
+        dir[i] = -1;
+    }
+    cur[0] = N + 1;
+    cur[N + 1] = N + 1;
+    dir[0] = 0;
+
+    int m = 0;
+    int n = 0;
+    while (m != 1)
+    {
+        /*for (int i = 1; i <= N; i++)
+            std::cout << cur[i] << " ";
+        std::cout << std::endl;*/
+        m = N;
+
+        while ((cur[rev[m] + dir[m]] > m) && (m > 1))
+        {
+            dir[m] = -dir[m];
+            m = m - 1;
+        }
+        std::swap(cur[rev[m]], cur[rev[m] + dir[m]]);
+        std::swap(rev[cur[rev[m]]], rev[cur[rev[m] + dir[m]]]);
+    }
+    double duration = t.elapsed();
+
+    std::cout << std::endl << duration << std::endl;
+    TimeCount(duration);
+}
---
+++ lab2/PartialPermutation/PartialPermutation/PartialPermutation.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#define BOOST_TIMER_ENABLE_DEPRECATED
+#include <boost/timer.hpp>
+#include <vector>
+#include <algorithm>
+
+const int N = 3;
+const int K = 2;
+
+void NextCombination(std::vector<int>& v)
+{
+    int i = K - 1;
+    while (true)
+    {
+        int max = N - K + 1 + i;
+        if (v[i] < max)
+        {
+            v[i]++;
+            while (i < K - 1)
+            {
+                i++;
+                v[i] = v[i - 1] + 1;
+            }
+            return;
+        }
+        i--;
+    }
+}
+
+int main()
+{
+    boost::timer t;
+    t.restart();
+
+    std::vector<int> v;
+    v.resize(K);
+    for (int i = 0; i < K; i++)
+    {
+        v[i] = i + 1;
+    };
+
+    while (true)
+    {
+        do
+        {
+            for (int i = 0; i < v.size(); i++)
+            {
+                std::cout << v[i] << " ";
+            };
+            std::cout << std::endl;
+        } while (std::next_permutation(v.begin(), v.end()));
+        if (v[0] == N - K + 1)
+        {
+            break;
+        };
+        NextCombination(v);
+    };
+
+    double duration = t.elapsed();
+    std::cout << std::endl << duration << std::endl;
+}
+
+/*
+
+Time Test
+K = 5
+
+N = 10: 0.147 s
+N = 20: 9.197 s
+N = 50: 21 min
+N = 100: 12.4 h
+
+*/
